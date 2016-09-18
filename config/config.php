f9bbdbc452c82e416dec706d8d6039f9961aa0b2
bigrocs <bigrocs@qq.com>
2016-09-18 16:58:39 +0800

    增加全局配置文件

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /*
+     * 产品配置
+     * 根据laravelCMF用户协议：
+     * 任何情况下使用laravelCMF均需获取官方授权（除了开源版本即laravelCMF），违者追究法律责任，授权联系：admin@bigrocs.vip
+     */
+    'productName' => 'laravelCMF',                      // 产品名称
+    'currentVersion' => '3.2.1',                        // 当前版本号
+    'websiteDomain' => 'http://www.bircos.vip',      // 官方网址
+
+];
